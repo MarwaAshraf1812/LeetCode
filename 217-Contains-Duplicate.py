f91a02b5f6ad96a420517c22e4cbb807fd8e7e13
marwa ashraf <126331254+MarwaAshraf1812@users.noreply.github.com>
2024-06-26 18:19:35 +0300

    [217] [Time Beats: 86.47%] [Memory Beats: 57.60%] - LeetPush

---
+++ 217-Contains-Duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        set_nums = set()
+
+        for i in nums:
+            if i in set_nums: return True
+            else: set_nums.add(i)
+        return False
