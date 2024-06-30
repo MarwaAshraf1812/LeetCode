f7efefb290f7194d403661cc754f9b519eeecf30
marwa ashraf <126331254+MarwaAshraf1812@users.noreply.github.com>
2024-06-30 20:38:46 +0300

    [169] [Time Beats: 98.87%] [Memory Beats: 58.80%] - LeetPush

---
+++ 169-Majority-Element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        n = len(nums)
+        """it appears more than half the time"""
+        return nums[n/2]
+        
