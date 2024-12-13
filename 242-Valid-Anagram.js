60b6cdd883cc039a04fad2a12cbe12fb731288a7
marwa ashraf <126331254+MarwaAshraf1812@users.noreply.github.com>
2024-12-13 23:24:08 +0200

    [242] [Time Beats: 60.89%] [Memory Beats: 57.51%] - LeetPush

--- 242-Valid-Anagram.js
+++ 242-Valid-Anagram.js
@@ -4,9 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let sorted_s = s.split('').sort().join('')
-    let sorted_t = t.split('').sort().join('')
+    // let sorted_s = s.split('').sort().join('')
+    // let sorted_t = t.split('').sort().join('')
 
-    if (sorted_s === sorted_t) return true
-    return false
+    // if (sorted_s === sorted_t) return true
+    // return false
+
+    if (s.length !== t.length) return false
+
+    /*to track the frequency of each char in string s
+     - frequency  - of each character refers to how many
+    times each character appears in a given string.*/
+    let count =  new Map();
+
+    for (let char of s) {
+        count.set(char, (count.get(char) || 0)+ 1)
+    }
+
+    for (let char of t) {
+        if (!count.has(char)) return false
+        count.set(char, count.get(char) - 1)
+        if (count.get(char) === 0) count.delete(char)
+    }
+
+    return count.size === 0;
 };
