f40b194e40937dd83e5aac6e1390c740e864c683
marwa ashraf <126331254+MarwaAshraf1812@users.noreply.github.com>
2024-12-16 17:07:24 +0200

    [27] [Time Beats: 100.00%] [Memory Beats: 63.52%] - LeetPush

---
+++ 27-Remove-Element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let pointer = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[pointer] = nums[i];
+            pointer++;
+        }
+    }
+
+    return pointer;
+    
+};
